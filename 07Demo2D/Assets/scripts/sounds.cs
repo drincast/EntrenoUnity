eb5aebef7479530a2ed970ede8dbf2063dac29c5
drincastX <drincast@gmail.com>
2019-04-02 22:05:17 -0500

    CApitulo de animación, inicio de animando la escena de mikka

---
+++ 07Demo2D/Assets/scripts/sounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sounds : MonoBehaviour
+{
+    public bool PlaySound()
+    {
+        bool res = false;
+
+        GetComponent<AudioSource>().Play();
+
+        res = true;
+        return res;
+    }
+}

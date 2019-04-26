c918792b9dabb260352f4acf856be9deff46a044
drincast <drincast@gmail.com>
2019-04-25 20:03:59 -0500

    se inicia con capitulo de programación de videojuegos en unity

---
+++ 14IntroProgramacion/Assets/scripts/Variables.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string saludo = "Saludo del code";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.saludo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

e10d8b1900ab8cdea04374203c9d13bafb48fc09
drincast <drincast@gmail.com>
2019-05-03 17:53:40 -0500

    Se inicia la seccion 17, juego en 2d

---
+++ 2Fast2Furious/Assets/script/MotorCarretera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorCarretera : MonoBehaviour
+{
+    public float velocidad;
+    public bool inicioJuego;
+    public bool finJuego;
+
+    public GameObject goContenedorCalle;
+    public GameObject[] vecContenedorCalle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.InicioJuego();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.inicioJuego && !this.finJuego){
+            transform.Translate(Vector3.down * this.velocidad * Time.deltaTime);
+        }        
+    }
+
+    void InicioJuego(){
+        this.goContenedorCalle = GameObject.Find("ContenedorCalle");
+        this.VelocidadMotorCarretera();
+        BuscoCalles();
+    }
+
+    void BuscoCalles(){
+        this.vecContenedorCalle = GameObject.FindGameObjectsWithTag("Calle");
+
+        for (int i = 0; i < this.vecContenedorCalle.Length; i++)
+        {
+            this.vecContenedorCalle[i].gameObject.transform.parent = this.goContenedorCalle.transform;
+            this.vecContenedorCalle[i].gameObject.SetActive(false);
+            this.vecContenedorCalle[i].gameObject.name = string.Format("CalleOFF_" + i);
+        }
+    }
+
+    void VelocidadMotorCarretera(){
+        this.velocidad = 5;
+    }
+}

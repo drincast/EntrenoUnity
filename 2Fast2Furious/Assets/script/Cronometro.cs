eafd9a40d305bd3e11ebae393fe88d2ce42c9223
drincastX <drincast@gmail.com>
2019-05-06 12:03:23 -0500

    2fast2fourious se realiza mecanica de conteo y distancia

---
+++ 2Fast2Furious/Assets/script/Cronometro.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cronometro : MonoBehaviour
+{
+    public GameObject goMotorCarretera;
+    public MotorCarretera scpMotorcarretera;
+
+    public float tiempo;
+    public float distancia;
+    public Text txtTiempo;
+    public Text txtDistacia;
+
+
+
+    void InicializarObjetos()
+    {
+        this.goMotorCarretera = GameObject.Find("MotorCarretera");
+        this.scpMotorcarretera = this.goMotorCarretera.GetComponent<MotorCarretera>();
+
+        this.txtTiempo.text = "2:00";
+        this.txtDistacia.text = "0";
+        this.tiempo = 30;
+    }
+
+    void CalcularTiempoDistacia()
+    {
+        this.distancia += Time.deltaTime * this.scpMotorcarretera.velocidad;
+        this.txtDistacia.text = ((int)this.distancia).ToString();
+
+        this.tiempo -= Time.deltaTime;
+        int minutos = (int)this.tiempo / 60;
+        int segundos = (int)this.tiempo % 60;
+        this.txtTiempo.text = string.Format("{0}:{1}", minutos.ToString(), segundos.ToString().PadLeft(2, '0'));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InicializarObjetos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.scpMotorcarretera.inicioJuego && this.scpMotorcarretera.finJuego == false)
+        {
+            CalcularTiempoDistacia();
+        }
+
+        if(this.tiempo <= 0 && this.scpMotorcarretera.finJuego == false)
+        {
+            this.scpMotorcarretera.finJuego = true;
+        }
+    }
+}

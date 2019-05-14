ddc2ed8c5926cbc352b771a7eef7cffecc9c6cc4
drincastX <drincast@gmail.com>
2019-05-13 21:26:17 -0500

    se realiza en proyecto test un codigo diferente para el disparo

--- test/test2d/Assets/scripts/test01/Bala.cs
+++ test/test2d/Assets/scripts/test01/Bala.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject goBala;
+
+    public Bala(Vector3 posicion)
     {
-        
+        this.goBala = new GameObject("bala");
+        SpriteRenderer sri = this.goBala.AddComponent<SpriteRenderer>();
+        sri.sprite = Resources.Load<Sprite>("sprites/bala_14x18");
+        //TODO: mejorar los numeros constantes, realizar algun tipo dede calculo para que se ajusten a cualquier tamaño
+        this.goBala.transform.position = new Vector3(posicion.x, posicion.y, posicion.z);
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool Desplazamiento()
     {
-        
+        bool res = false;
+
+        try
+        {
+            this.goBala.SetActive(true);
+            res = true;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(string.Format("Error en Bala: {0}", ex));
+            throw;
+        }
+
+        return res;
     }
 }
--- test/test2d/Assets/scripts/test01/Nave.cs
+++ test/test2d/Assets/scripts/test01/Nave.cs
@@ -4,30 +4,67 @@ using UnityEngine;
 
 class Disparo
 {
-    public GameObject balaIzq;
-    public GameObject balaDer;
+    //public GameObject balaIzq;
+    //public GameObject balaDer;
 
-    private Sprite CrearBala()
+    //private Sprite CrearBala()
+    //{
+    //    return Resources.Load<Sprite>("sprites/bala_14x18");
+    //}
+
+    //public Disparo(Vector3 posicion)
+    //{
+    //    this.balaIzq = new GameObject("bala");
+    //    SpriteRenderer sri = this.balaIzq.AddComponent<SpriteRenderer>();
+    //    sri.sprite = CrearBala();
+    //    //this.balaIzq.transform.position = new Vector3(posicion.x, posicion.y, posicion.z);
+    //    //TODO: mejorar los numeros constantes, realizar algun tipo dede calculo para que se ajusten a cualquier tamaño
+    //    this.balaIzq.transform.position = new Vector3(posicion.x - 0.401166f, posicion.y + 0.556f, posicion.z);
+
+    //    this.balaDer = new GameObject("bala");
+    //    SpriteRenderer srd = this.balaDer.AddComponent<SpriteRenderer>();
+    //    srd.sprite = CrearBala();
+    //    this.balaDer.transform.position = new Vector3(posicion.x + 0.401834f, posicion.y + 0.556f, posicion.z);
+
+    //    this.balaIzq.SetActive(true);
+    //    this.balaDer.SetActive(true);
+    //}
+
+    //public GameObject[] ObtenerDisparo()
+    //{
+    //    GameObject[] vec = { this.balaIzq, this.balaDer };
+
+    //    return vec;
+    //}
+
+    public Bala balaIzq;
+    public Bala balaDer;
+
+    //TODO: para crear las balas, por el momento hago referencia a que se disparan dos balas, aqui ya viene el concepto de arma,
+    //      esto se debe aque he tomado dos atributos de tipo bala que tienen el concepto de bala izquierda y bala derecha
+    //      analizar para mejorar el código orientado a objetos, para que quede mas claro.
+    private bool CrearBalas(Vector3 posicion)
     {
-        return Resources.Load<Sprite>("sprites/bala_14x18");
+        bool res = false;
+
+        try
+        {
+            this.balaIzq = new Bala(new Vector3(posicion.x - 0.401166f, posicion.y + 0.556f, posicion.z));
+            this.balaDer = new Bala(new Vector3(posicion.x + 0.401834f, posicion.y + 0.556f, posicion.z));
+            res = true;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(string.Format("Error en Nave (Disparo): {0}", ex));
+            throw;
+        }
+
+        return res;
     }
 
     public Disparo(Vector3 posicion)
     {
-        this.balaIzq = new GameObject("bala");
-        SpriteRenderer sri = this.balaIzq.AddComponent<SpriteRenderer>();
-        sri.sprite = CrearBala();
-        //this.balaIzq.transform.position = new Vector3(posicion.x, posicion.y, posicion.z);
-        //TODO: mejorar los numeros constantes, realizar algun tipo dede calculo para que se ajusten a cualquier tamaño
-        this.balaIzq.transform.position = new Vector3(posicion.x - 0.401166f, posicion.y + 0.556f, posicion.z);
-        
-        this.balaDer = new GameObject("bala");
-        SpriteRenderer srd = this.balaDer.AddComponent<SpriteRenderer>();
-        srd.sprite = CrearBala();
-        this.balaDer.transform.position = new Vector3(posicion.x + 0.401834f, posicion.y + 0.556f, posicion.z);
-
-        this.balaIzq.SetActive(true);
-        this.balaDer.SetActive(true);
+        this.CrearBalas(posicion);
     }
 
     public GameObject[] ObtenerDisparo()
@@ -40,6 +77,84 @@ class Disparo
 
 public class Nave : MonoBehaviour
 {
+    //private List<GameObject[]> lstGoDisparo = null;
+
+    //public bool MoverNave(float movimiento)
+    //{
+    //    bool res = false;
+
+    //    try
+    //    {
+    //        this.transform.Translate(Vector2.right * movimiento * Time.deltaTime);
+    //        res = true;
+    //    }
+    //    catch (System.Exception ex)
+    //    {
+    //        Debug.Log(string.Format("Error en Nave: {0}", ex));
+    //        throw;
+    //    }
+
+    //    return res;
+    //}
+
+    //public bool Disparar(Vector3 posicion)
+    //{
+    //    bool res = false;
+
+    //    try
+    //    {
+    //        this.CrearDisparo(posicion);
+    //        res = true;
+    //    }
+    //    catch (System.Exception ex)
+    //    {
+    //        Debug.Log(string.Format("Error en Nave: {0}", ex));
+    //        throw;
+    //    }
+
+    //    return res;
+    //}
+
+    //private List<GameObject[]> ObtenerLstGoDisparo()
+    //{
+    //    try
+    //    {
+    //        if(this.lstGoDisparo == null)
+    //        {
+    //            this.lstGoDisparo = new List<GameObject[]>();
+    //        }
+    //    }
+    //    catch (System.Exception ex)
+    //    {
+    //        Debug.Log(string.Format("Error en Nave: {0}", ex));
+    //        throw;
+    //    }
+
+    //    return this.lstGoDisparo;
+    //}
+
+    //private bool CrearDisparo(Vector3 posicion)
+    //{
+    //    bool res = false;
+    //    Disparo disparo = null;
+
+    //    try
+    //    {
+    //        disparo = new Disparo(posicion);
+    //        ObtenerLstGoDisparo().Add(disparo.ObtenerDisparo());
+    //    }
+    //    catch (System.Exception ex)
+    //    {
+    //        Debug.Log(string.Format("Error en Nave: {0}", ex));
+    //        throw;
+    //    }
+
+    //    return res;
+    //}
+
+
+
+
     private List<GameObject[]> lstGoDisparo = null;
 
     public bool MoverNave(float movimiento)
@@ -82,7 +197,7 @@ public class Nave : MonoBehaviour
     {
         try
         {
-            if(this.lstGoDisparo == null)
+            if (this.lstGoDisparo == null)
             {
                 this.lstGoDisparo = new List<GameObject[]>();
             }

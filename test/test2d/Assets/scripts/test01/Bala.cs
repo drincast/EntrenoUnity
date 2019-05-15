94088660783d8a9e12a67efaa552b433d1f07a66
drincastX <drincast@gmail.com>
2019-05-14 22:15:35 -0500

    modificacion en proyecto test

--- test/test2d/Assets/scripts/test01/Bala.cs
+++ test/test2d/Assets/scripts/test01/Bala.cs
@@ -12,9 +12,31 @@ public class Bala : MonoBehaviour
         SpriteRenderer sri = this.goBala.AddComponent<SpriteRenderer>();
         sri.sprite = Resources.Load<Sprite>("sprites/bala_14x18");
         //TODO: mejorar los numeros constantes, realizar algun tipo dede calculo para que se ajusten a cualquier tamaño
-        this.goBala.transform.position = new Vector3(posicion.x, posicion.y, posicion.z);
+        
+        this.Materializar(posicion);
+    }
 
-        this.Desplazamiento();
+    private bool Materializar(Vector3 posicion)
+    {
+        bool res = false;
+
+        this.goBala.SetActive(true);
+        this.goBala.transform.position = new Vector3(posicion.x, posicion.y, posicion.z);
+        res = true;
+
+        //try
+        //{
+        //    this.goBala.SetActive(true);
+        //    StartCoroutine("DesplazarBala");
+        //    res = true;
+        //}
+        //catch (System.Exception ex)
+        //{
+        //    Debug.Log(string.Format("Error en Bala: {0}", ex));
+        //    throw ex;
+        //}
+
+        return res;
     }
 
     public bool Desplazamiento()

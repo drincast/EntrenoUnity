2d015fc68f3bd8beb0e5660e8c054c33557ee3a4
rdorozco <rdorozco@imbanaco.com.co>
2019-05-27 10:56:46 -0500

    Se finaliza sección 19 mecanim

---
+++ IntroMecanim/Assets/Scripts/SCETransicionesScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCETransicionesScript : MonoBehaviour
+{
+    public Animator animController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animController = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Avanzar();
+        this.Salto();
+        this.Viento();
+        this.Escala();
+    }
+
+    void Avanzar(){
+        if(Input.GetButton("Fire1")){
+            this.animController.SetBool("Avanzar", true);
+        }
+
+        if(Input.GetButton("Fire2")){
+            this.animController.SetBool("Avanzar", false);
+        }
+    }
+
+    void Salto(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            this.animController.SetTrigger("Salto");
+        }
+    }
+
+    void Viento(){
+        if(Input.GetKeyDown(KeyCode.V)){
+            this.animController.SetFloat("Viento", 0.5f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.B)){
+            this.animController.SetFloat("Viento", 0.0f);
+        }
+    }
+
+    void Escala(){
+        if(Input.GetKeyDown(KeyCode.S)){
+            this.animController.SetInteger("Escala", 4);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D)){
+            this.animController.SetInteger("Escala", 0);
+        }
+    }
+}

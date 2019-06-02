1399c40911f4fe2cb54da2d9a345ae4ccef8deb2
drincast <drincast@gmail.com>
2019-06-02 17:01:56 -0500

    Proyecto test, se realiza movimiento del fondo en ambos ejes, y para cada eje en sus dos direcciones, falta mejorar el posicionamiento inicial y final, realizar pruebas con varios tamaños de sprite, y mirar la posibilidad de identificar la cercania de la camara para que no dependa de una camara con una cercania afija, y mas cosas

--- test/test2d/Assets/scripts/MovFondo/01/MovFondo01.cs
+++ test/test2d/Assets/scripts/MovFondo/01/MovFondo01.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class MovFondo01 : MonoBehaviour
 {
+    public bool esEjeY;
+    public bool invertirMov;
+    
+
     public bool iniciar;
     public float posicionReferencia;
+    public float posicionReferenciaX;
     public float tamanioYcamara;
+    public float tamanioXcamara;
     public float margen;
 
     private void ImpInfo(string nombre, GameObject go)
@@ -26,15 +32,28 @@ public class MovFondo01 : MonoBehaviour
 
         try
         {
-            //el calculo del tamaño del eje y de la camara seria el yMax de la camara dividido por pixeles por unidad del sprite
-            // this.tamanioYcamara = GameObject.Find("Main Camera").GetComponent<Camera>().pixelRect.yMax / this.gameObject.GetComponent<SpriteRenderer>().sprite. pixelsPerUnit;
-            this.tamanioYcamara = GameObject.Find("Main Camera").GetComponent<Camera>().pixelRect.yMax / 100; //el 100 deberia ser this.gameObject.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit pero da otro valor, averiguar porque
+            if(this.esEjeY){
+                //el calculo del tamaño del eje y de la camara seria el yMax de la camara dividido por pixeles por unidad del sprite
+                // this.tamanioYcamara = GameObject.Find("Main Camera").GetComponent<Camera>().pixelRect.yMax / this.gameObject.GetComponent<SpriteRenderer>().sprite. pixelsPerUnit;
+                this.tamanioYcamara = GameObject.Find("Main Camera").GetComponent<Camera>().pixelRect.yMax / 100; //el 100 deberia ser this.gameObject.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit pero da otro valor, averiguar porque                                
 
-            this.ImpInfo(this.name, this.gameObject);
+                //Este calculo se debe hacer con el sprite completo, el sprite debe tener la misma forma en el cuadro inicial y el final
+                this.posicionReferencia = (this.GetComponent<SpriteRenderer>().size.y / 2) - (this.tamanioYcamara / 2);                
 
-            //Este calculo se debe hacer con el sprite completo, el sprite debe tener la misma forma en el cuadro inicial y el final
-            this.posicionReferencia = (this.GetComponent<SpriteRenderer>().size.y/2) - (this.tamanioYcamara / 2);
-            this.margen = 0f;
+                this.ImpInfo(this.name, this.gameObject);
+                Debug.Log(string.Format("this.tamanioXcamara: {0}, this.posicionReferenciaX: {1}", this.tamanioXcamara, this.posicionReferenciaX));
+
+                this.margen = 0f;                
+            }
+            else{
+                this.tamanioXcamara = GameObject.Find("Main Camera").GetComponent<Camera>().pixelRect.xMax / 100;
+                this.posicionReferenciaX = (this.GetComponent<SpriteRenderer>().size.x / 2) - (this.tamanioXcamara / 2);
+            }
+
+            if(this.invertirMov){
+
+            }
+
             res = true;
             return res;
         }
@@ -44,8 +63,7 @@ public class MovFondo01 : MonoBehaviour
         }
     }
 
-    public bool MoverEnY(float direccion, float desplazamiento)
-    {
+    public bool MoverEnYHaciaAbajo(float direccion, float desplazamiento){
         bool res = false;
 
         try
@@ -69,6 +87,149 @@ public class MovFondo01 : MonoBehaviour
                 gameObject.transform.position = new Vector3(0, this.posicionReferencia, 0);
             }
 
+            res = true;    
+        }
+        catch (System.Exception ex)
+        {            
+            throw ex;
+        }
+
+        return res;
+    }
+
+    public bool MoverEnYHaciaArriba(float direccion, float desplazamiento){
+        bool res = false;
+
+        try
+        {
+            if (this.posicionReferencia > this.transform.position.y)
+            {
+                gameObject.transform.position = new Vector3(0, (this.transform.position.y + (direccion * desplazamiento)), 0);
+                //Debug.Log(string.Format("position - {0} ", gameObject.GetComponent<SpriteRenderer>().transform.position.ToString()));
+            }
+            else
+            {
+                //el calculo es aprox (el tamaño de la (spriteY/2)-(la mitad de la camara en y)
+                float posIni = (this.GetComponent<SpriteRenderer>().size.y/2) - (this.tamanioYcamara/2);
+                //this.ImpInfo(gameObject.name, gameObject);                
+                Debug.Log(string.Format("this.tamanioYcamara - {0}, posIni: {1}", this.tamanioYcamara, posIni));
+                //gameObject.transform.position = new Vector3(0, this.tamanioYcamara, 0);
+                gameObject.transform.position = new Vector3(0, -this.posicionReferencia, 0);
+            }
+
+            res = true;    
+        }
+        catch (System.Exception ex)
+        {            
+            throw ex;
+        }
+
+        return res;
+    }
+
+    public bool MoverEnXHaciaIzquierda(float direccion, float desplazamiento){
+        bool res = false;
+
+        try
+        {
+            if (-this.posicionReferenciaX < this.transform.position.x)
+            {
+                gameObject.transform.position = new Vector3((this.transform.position.x + (direccion * desplazamiento)), 0, 0);
+            }
+            else
+            {
+                gameObject.transform.position = new Vector3(this.posicionReferenciaX, 0, 0);
+            }
+
+            res = true;    
+        }
+        catch (System.Exception ex)
+        {            
+            throw ex;
+        }
+
+        return res;
+    }
+
+    public bool MoverEnXHaciaDerecha(float direccion, float desplazamiento){
+        bool res = false;
+
+        try
+        {
+            Debug.Log(string.Format("direccion: {0}, desplazamiento: {1}, this.posicionReferenciaX: {2}, this.transform.position.x: {3}"
+                    , direccion, desplazamiento, this.posicionReferenciaX, this.transform.position.x));
+            if (this.posicionReferenciaX > this.transform.position.x)
+            {
+                gameObject.transform.position = new Vector3((this.transform.position.x + (direccion * desplazamiento)), 0, 0);
+                //Debug.Log(string.Format("position - {0} ", gameObject.GetComponent<SpriteRenderer>().transform.position.ToString()));
+            }
+            else
+            {
+                gameObject.transform.position = new Vector3(-this.posicionReferenciaX, 0, 0);
+            }
+
+            res = true;    
+        }
+        catch (System.Exception ex)
+        {            
+            throw ex;
+        }
+
+        return res;
+    }
+
+    /*
+        Desplazamiento del fondo en eje y, si this.invertirMov no esta chequeado va hacia arriba, 
+        sino va hacia abajo 
+        si thi
+     */
+    public bool MoverEnY(float desplazamiento)
+    {
+        bool res = false;
+
+        try
+        {
+            short invertir = 1; //hacia arriba
+
+            if(this.invertirMov){
+                invertir = -1; //hacia abajo
+            }
+
+            if(invertir == 1){
+                this.MoverEnYHaciaArriba(invertir, desplazamiento);
+            }
+            else{
+                this.MoverEnYHaciaAbajo(invertir, desplazamiento);             
+            }
+
+            res = true;
+            return res;
+        }
+        catch (System.Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public bool MoverEnX(float desplazamiento)
+    {
+        bool res = false;
+
+        try
+        {
+            short invertir = 1; //hacia derecha
+
+            if(this.invertirMov){
+                invertir = -1; //hacia izquierda
+            }
+
+            if(invertir == 1){                
+                this.MoverEnXHaciaDerecha(invertir, desplazamiento);
+            }
+            else{
+                this.MoverEnXHaciaIzquierda(invertir, desplazamiento);             
+            }
+
             res = true;
             return res;
         }
@@ -85,7 +246,12 @@ public class MovFondo01 : MonoBehaviour
 
     private void Update() {
         if(iniciar){
-            this.MoverEnY(-1, 0.1f);
+            if(this.esEjeY){
+                this.MoverEnY(0.1f);
+            }
+            else{                
+                this.MoverEnX(0.1f);
+            } 
         }
     }
 }

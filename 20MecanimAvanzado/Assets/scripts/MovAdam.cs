4d77c0cb7159869abcffc16136371b9b952893cb
rdorozco <rdorozco@imbanaco.com.co>
2019-06-04 10:33:38 -0500

    Seccion 20 Mecanim Avanzado, se finaliza sección, falta hacer el demo de adam para que se mueva a los lados

---
+++ 20MecanimAvanzado/Assets/scripts/MovAdam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovAdam : MonoBehaviour
+{
+    public Animator anim;
+    public float ejeVertical;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.ejeVertical = Input.GetAxis("Vertical");
+        anim.SetFloat("caminar", this.ejeVertical);
+        anim.SetFloat("caminarAtras", this.ejeVertical);
+    }
+}

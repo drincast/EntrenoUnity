e7c107491d83d07702ac13f9415abcb025c4358c
rdorozco <rdorozco@imbanaco.com.co>
2019-09-17 16:46:52 -0500

    Se continua con demo de scroll view

---
+++ test/test2d/Assets/scripts/IU/ShopScrollList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite icon;
+    public float price = 1f;
+}
+
+public class ShopScrollList : MonoBehaviour
+{
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public ShopScrollList otherShop;
+    public Text myGoldisplay;
+    public SimpleObjectPool buttonObjectPool;
+    public float gold = 20f;
+
+    private void AddButtons()
+    {
+        for(int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+        }
+
+    }
+}
+
